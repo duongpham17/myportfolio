ee228900afacf9e20fd458eccc1fe88c0d11d15e
Duong Pham <duongpham17@icloud.com>
2020-12-12 20:04:04 +0000

    completed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,31 +1,38 @@
 import './Navbar.scss';
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {logout} from '../../actions/authActions';
 import {RiAccountBoxLine, RiLogoutBoxRLine} from 'react-icons/ri';
 import {AiFillHome} from 'react-icons/ai';
+import {MdAccountBalance} from 'react-icons/md';
 
-const Navbar = ({logout, auth:{loggedOn}}) => {
-
+const Navbar = ({logout, auth:{loggedOn}}) => { 
     return (
         <header className="navbar">
             <nav>
                 <Link to="/"><AiFillHome className="icon_s_white"/></Link>
-                <Link to="/portfolio">My.Portfolio</Link>
+                <Link className="not-middle" to="/portfolio">My.Portfolio</Link>
+            </nav>
+
+            <nav>
+                <Link className="middle" to="/portfolio">My.Portfolio</Link>
             </nav>
 
             <nav>
                 {loggedOn ? 
-                <div className="logged-on">
-                    <Link to="/account"><RiAccountBoxLine className="icon_s_white"/> Account</Link>
-                    <Link to='/' onClick={() => logout()}><RiLogoutBoxRLine className="icon_s_white"/> Logout</Link>
+                <div className="logged-on-container">
+                    <button className="dropdown-btn"><MdAccountBalance className="icon_s_white"/></button>
+                    <div className="dropdown-content">
+                        <li><p><Link to="/account"><RiAccountBoxLine className="icon_s_white"/> Account</Link></p></li>
+                        <li><p><Link to='/' onClick={() => logout()}><RiLogoutBoxRLine className="icon_s_white"/> Logout</Link></p></li>
+                    </div>
                 </div>
                 : 
-                <Fragment>
+                <div>
                     <Link to="/login">Login</Link>
                     <Link to="/signup">Signup</Link>
-                </Fragment>
+                </div>
                 }
             </nav>
         </header>
